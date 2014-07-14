20bf443d78d7f5088f52d6b1e628dc60df564b00
Hiroyuki Yabe <yabe@cyber.t.u-tokyo.ac.jp>
2014-07-15 02:25:42 +0900

    画面外に消えたオブジェクトを破棄する機能を追加

---
+++ Assets/DestroyObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//画面外に消えたらオブジェクトを破棄
+	void OnBecameInvisible () 
+	{
+		Destroy(this.gameObject);
+	}
+
+}
